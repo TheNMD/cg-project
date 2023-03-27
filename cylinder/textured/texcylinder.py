5bfee64954878468f251aff3f7fd9b429efa23b6
TheNMD <manhdanog@gmail.com>
2023-03-27 22:28:34 +0700

    com

--- cylinder/textured/texcylinder.py
+++ cylinder/textured/texcylinder.py
@@ -148,6 +148,7 @@ class TexCylinder(object):
         self.uma.upload_uniform_scalar1f(phong_factor, 'phong_factor')
         
         self.uma.setup_texture("texture", "./textured/image/test.png")
+        # self.uma.setup_texture("texture", "./textured/image/test1.png")
         
         return self
 
